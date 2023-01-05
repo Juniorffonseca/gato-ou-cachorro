9cdf6e1cb4fca7cdc2171d71e57ef6ba9cd19096
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-01-05 19:06:44 -0300

    .

--- 01_primeiro_script.R
+++ 01_primeiro_script.R
@@ -5,7 +5,7 @@ library(tensorflow)
 library(reticulate)
 
 # Training
-setwd("C:/Users/anonb/Documents/Projeto2")
+setwd("C:/Users/anonb/Documents/Cats_Recognition")
 label_list <- dir("dados_brutos/")
 output_n <- length(label_list)
 save(label_list, file="label_list.R")
@@ -16,22 +16,52 @@ height <- 64
 target_size <- c(width, height)
 rgb <- 3 #color channels
 
-path <- "dados_brutos/"
+path_train <- "dados_brutos/"
+train_data_gen <- image_data_generator(rescale = 1/255, 
+                                       validation_split = .2)
 
-train_image_files_path <- file.path(path, "cats", "Training")
-valid_image_files_path <- file.path(path, "cats", "Test")
+train_images <- flow_images_from_directory(path_train,
+                                           train_data_gen,
+                                           subset = 'training',
+                                           target_size = target_size,
+                                           class_mode = "categorical",
+                                           shuffle=F,
+                                           seed = 2021)
 
-train_data_gen = image_data_generator(
-  rescale = 1/255
-)
+validation_images <- flow_images_from_directory(path_train,
+                                                train_data_gen, 
+                                                subset = 'validation',
+                                                target_size = target_size,
+                                                class_mode = "categorical",
+                                                seed = 2021)
 
-valid_data_gen <- image_data_generator(
-  rescale = 1/255
-)
 
-train_image_array_gen <- flow_images_from_directory(train_image_files_path, 
-                                                    train_data_gen,
-                                                    target_size = target_size,
-                                                    class_mode = 'categorical',
-                                                    classes = '',
-                                                    seed = 42)
+mod_base <- application_xception(weights = 'imagenet', 
+                                 include_top = FALSE, input_shape = c(71, 71, 3))
+freeze_weights(mod_base) 
+
+model_function <- function(learning_rate = 0.001, 
+                           dropoutrate=0.2, n_dense=1024){
+  
+  k_clear_session()
+  
+  model <- keras_model_sequential() %>%
+    mod_base %>% 
+    layer_global_average_pooling_2d() %>% 
+    layer_dense(units = n_dense) %>%
+    layer_activation("relu") %>%
+    layer_dropout(dropoutrate) %>%
+    layer_dense(units=output_n, activation="softmax")
+  
+  model %>% compile(
+    loss = "categorical_crossentropy",
+    optimizer = optimizer_adam(lr = learning_rate),
+    metrics = "accuracy"
+  )
+  
+  return(model)
+  
+}
+
+model <- model_function()
+
